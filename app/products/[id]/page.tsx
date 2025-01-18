7b786771ef84c61831062af12051caaf30d9b060
khomkrit0710 <lnwmin20@gmail.com>
2025-01-19 00:28:17 +0700

    full_V2.9

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,1 +1,10 @@
-export { default } from './ProductPage'
+import ProductDetails from './ProductDetails'
+ 
+export default async function Page({
+  params,
+}: {
+  params: { id: string }
+  searchParams?: { [key: string]: string | string[] | undefined }
+}) {
+  return <ProductDetails productId={params.id} />
+}
