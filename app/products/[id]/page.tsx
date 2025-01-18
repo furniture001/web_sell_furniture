deaabcf0f80f00ae3a6b6eafc9902ddbef5c5095
khomkrit0710 <lnwmin20@gmail.com>
2025-01-19 00:05:46 +0700

    full_V2.2

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,13 +1,19 @@
-// pages/products/[id]/page.tsx
-
+import { Metadata } from 'next'
 import ProductDetails from './ProductDetails'
 
-interface Props {
+interface PageProps {
   params: {
     id: string
   }
+  searchParams: { [key: string]: string | string[] | undefined }
 }
 
-export default function ProductPage({ params }: Props) {
+export function generateMetadata({ params }: PageProps): Metadata {
+  return {
+    title: `สินค้า - ${params.id}`,
+  }
+}
+
+export default function ProductPage({ params, searchParams }: PageProps) {
   return <ProductDetails productId={params.id} />
 }
